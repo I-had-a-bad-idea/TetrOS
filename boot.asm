ec95f9f08d6e5584dc54b71c24be7fcad3ab83b0
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-02-15 13:26:39 +0100

    Print string

--- boot.asm
+++ boot.asm
@@ -1,17 +1,25 @@
+[org 0x7c00] ; set origin to offset
+
 ; enable character "printing"
 mov ah, 0x0e    ; function number = 0Eh : Display Character
 
-; set al to 64 (1 before 'A')
-mov al, 64
-print_alphabet:
-    ; increase al by one
-    inc al          ; AL = code of character to display
-    int 0x10        ; call INT 10h, BIOS video service
+mov bx, hello_world_string ; set bx to the flag pointer
 
-    cmp al, 90
-    jne print_alphabet
+print_string:
+    mov al, [bx] ; get char 
+    cmp al, 0 ; check if end of string
+    je end    ; jump to end label
+    int 0x10 ; BIOS interrupt for "printing"
+    inc bx   ; next char (increment pointer)
+    jmp print_string ; loop through all of string
+
+end:
+
 
 jmp $
+hello_world_string:
+    db "Hello World", 0
+
 ; define 510 bytes + 2 bytes after = 512 bytes (boot sector)
 times 510 - ($-$$) db 0 ; ($-$$) = the code before
 db 0x55, 0xaa
