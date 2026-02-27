30c3d83cf3fa7ad588aa7e1971f7e8a15dc98d3c
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-02-27 20:46:27 +0100

    Print "Hello World" from the kernel

--- kernel.c
+++ kernel.c
@@ -1,6 +1,27 @@
+// #include "idt.h"
 #include "kernel.h"
 
+void print_char(char c) {
+
+    unsigned short* video_memory = (unsigned short*)0xB8000;
+
+    if (c == '\n') {
+        // move to next line
+        cursor_position += VIDEO_WIDTH - (cursor_position % VIDEO_WIDTH);
+    } else {
+        video_memory[cursor_position++] = (WHITE_ON_BLACK << 8) | c;
+    }
+}
+
+void print_string(const char* str) {
+    while (*str) {
+        print_char(*str++);
+    }
+}
+
 void main(){
-    *(char*)0xb8000 = 'X';
+    // init_idt();
+    
+    print_string("Hello World,\nthis is the kernel!");
     return;
 }
--- kernel.h
+++ kernel.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "stdint.h"
 
-#define VIDEO_MEMORY_ADDRESS 0xb8000
+#define VIDEO_MEMORY 0xB8000
+#define WHITE_ON_BLACK 0x0F
+#define VIDEO_WIDTH 80
+
+uint32_t cursor_position = 0;
 
 void print_char(char c);
 void print_string(const char* str);
