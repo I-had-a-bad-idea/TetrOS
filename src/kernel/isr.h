b76458069f97b8ac03fd95daa3501b98c2ca5815
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-03-04 17:12:08 +0100

    Fix Registers (remove invalid)

--- src/kernel/isr.h
+++ src/kernel/isr.h
@@ -8,7 +8,7 @@ typedef struct Registers {
   uint32_t ds;
   uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;  // pushed by pusha
   uint32_t interrupt_number, error_code;  // interrupt number and error code
-  uint32_t eip, cs, eflags, useresp, ss;  // pushed by the processor automatically
+  uint32_t eip, cs, eflags;  // pushed by the processor automatically
 } __attribute__((packed)) Registers;
 
 typedef void (*ISR_Handler)(Registers* regs);
