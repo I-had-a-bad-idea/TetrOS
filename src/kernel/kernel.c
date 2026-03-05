4d436d2cee1ffbd1c9e5d6a37d540d5c86ab72c7
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-03-05 18:00:33 +0100

    print_int()

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -21,9 +21,31 @@ void print_string(const char* str) {
 }
 
 void print_int(int n) {
-    print_string_literal("Int print");
-    // TODO: Implement actual printing of ints
+    if (n == 0) {
+        print_char('0');
+        return;
+    }
+
+    // Handle negative numbers
+    if (n < 0) {
+        print_char('-');
+        n = -n;
+    }
+
+    // Hanle actual number
+    char buffer[10];
+    int i = 0;
+
+    while (n > 0) {
+        buffer[i++] = '0' + (n % 10);
+        n /= 10;
+    }
+
+    for (int j = i - 1; j >= 0; j--){
+        print_char(buffer[j]);
+    }
 }
+
 
 void main(){
     print_string_literal("Kernel started!\nSetting up the IDT...\n");
@@ -31,9 +53,10 @@ void main(){
     print_string_literal("IDT-setup successful!\nSetting up the ISR...\n");
     init_isr();
     print_string_literal("ISR-setup successful!\n");
-    
-    
-    // print_string_literal("Triggering invalid opcode..\n");    
-    // int x = 1 / 0;
+
+    print_int(-12345);
+    print_char(' ');
+    print_int(54321);
+
     return;
 }
