58746ea83eadb835d9e92861c592e28b4d36a7ff
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-03-07 15:24:37 +0100

    PIC_mask and PIC_unmask

--- src/kernel/pic.c
+++ src/kernel/pic.c
@@ -16,5 +16,31 @@ void PIC_disable() {
     outb(PIC1_DATA, 0xff); // Mask all master PIC interrupts
     outb(PIC2_DATA, 0xff); // Mask all slave PIC interrupt
 }
-void PIC_mask(int irq);
-void PIC_unmask(int irq);
+void PIC_mask(int irq) {
+    uint16_t port;
+    uint8_t value;
+
+    if (irq < 8) { // master PIC
+        port = PIC1_DATA;
+    } else { // slave PIC
+        port PIC2_DATA;
+        irq -= 8; // PIC2 has its own 8 interrupts
+    }
+    
+    value = inb(port) | (1 << irq);
+    outb(port, value);
+}
+void PIC_unmask(int irq) {
+    uint16_t port;
+    uint8_t value;
+
+    if (irq < 8) { // master PIC
+        port = PIC1_DATA;
+    } else { // slave PIC
+        port PIC2_DATA;
+        irq -= 8; // PIC2 has its own 8 interrupts
+    }
+    
+    value = inb(port) & ~(1 << irq);
+    outb(port, value);
+}
