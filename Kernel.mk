5c49b75ef006175491081956b6e6bd76dbd3a5ec
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-03-13 15:04:36 +0100

    Include Tetris in Makefile + cleaner Makefile

--- Kernel.mk
+++ Kernel.mk
@@ -1,48 +1,56 @@
-# Default target
-all: binaries/TetrOS.bin
+# Kernel object files
+OBJ = \
+kernel_object_files/kernel.o \
+kernel_object_files/idt.o \
+kernel_object_files/isr.o \
+kernel_object_files/isr_gen.o \
+kernel_object_files/io.o \
+kernel_object_files/pic.o \
+kernel_object_files/irq.o
 
-# Create build directories if needed
+# Tetris source files
+TETRIS_SRC := $(wildcard src/tetris/*.c)
+
+# Object files generated from them
+TETRIS_OBJ := $(patsubst src/tetris/%.c,kernel_object_files/%.o,$(TETRIS_SRC))
+
+CFLAGS = -ffreestanding -m32 -g -I src
+
+all: TetrOS.bin
+
+# Ensure directories exist
 kernel_object_files:
 	mkdir -p kernel_object_files
 
 binaries:
 	mkdir -p binaries
 
-# Compile kernel.c
-kernel_object_files/kernel.o: src/kernel/kernel.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
 
-# Compile idt.c
-kernel_object_files/idt.o: src/kernel/idt.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
+# Compile kernel source files
+kernel_object_files/%.o: src/kernel/%.c | kernel_object_files
+	i686-elf-gcc $(CFLAGS) -c $< -o $@
 
-# Compile isr.c
-kernel_object_files/isr.o: src/kernel/isr.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
+# Compile Tetris source files
+kernel_object_files/%.o: src/tetris/%.c | kernel_object_files
+	i686-elf-gcc $(CFLAGS) -c $< -o $@
 
-# Compile isr_gen.c
-kernel_object_files/isr_gen.o: src/kernel/isr_gen.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
 
-# Compile io.c
-kernel_object_files/io.o: src/kernel/io.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
+# Link full kernel
+binaries/full_kernel.bin: \
+	binaries/kernel_entry.o \
+	$(OBJ) \
+	binaries/isr.o \
+	binaries/io.o \
+	$(TETRIS_OBJ) \
+	| binaries
 
-# Compile pic.c
-kernel_object_files/pic.o: src/kernel/pic.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
-
-# Compile irq.c
-kernel_object_files/irq.o: src/kernel/irq.c | kernel_object_files
-	i686-elf-gcc -ffreestanding -m32 -g -c $< -o $@
-
-# Link all object files to full_kernel.bin
-binaries/full_kernel.bin: binaries/kernel_entry.o kernel_object_files/kernel.o kernel_object_files/idt.o kernel_object_files/isr.o binaries/isr.o kernel_object_files/isr_gen.o binaries/io.o kernel_object_files/io.o kernel_object_files/pic.o kernel_object_files/irq.o | binaries
 	i686-elf-ld -o $@ -T linker.ld $^ --oformat binary
 
-# Combine into TetrOS.bin
-binaries/TetrOS.bin: binaries/boot.bin binaries/full_kernel.bin  binaries/zeroes.bin
+
+# Combine bootloader + kernel
+TetrOS.bin: binaries/boot.bin binaries/full_kernel.bin binaries/zeroes.bin | binaries
 	cat $^ > $@
+
 
 # Clean
 clean:
--- VirtualBox.mk
+++ VirtualBox.mk
@@ -2,4 +2,4 @@ all: disk.img
 
 disk.img:
 	dd if=/dev/zero of=disk.img bs=1M count=1
-	dd if=binaries/TetrOS.bin of=disk.img conv=notrunc
+	dd if=TetrOS.bin of=disk.img conv=notrunc
