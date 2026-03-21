64cc8e971cc1c2ad2294c72f88f8c60b710a0c27
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-03-21 17:15:21 +0100

    More robust kernel_entry

--- src/bootloader/kernel_entry.asm
+++ src/bootloader/kernel_entry.asm
@@ -1,4 +1,13 @@
+section .text.start
+
 [bits 32]
 [extern main]
-call main ; call main inside kernel
-jmp $
+global _start
+
+_start:
+    ; Write to video memory to show that we are in protected mode
+    mov eax, 0xb8000 ; video memory address
+    mov word [eax], 0x0f41 ; white 'A' on black background
+
+    call main ; call main inside kernel
+    jmp $
